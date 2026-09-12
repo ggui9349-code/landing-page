fde62b6bb11be50d93181c605d956784cb98c3ce
ggui9349-code <ggui9349@gmail.com>
2026-09-12 17:17:28 -0300

    fix: resolve missing framer motion types

---
+++ types/framer-motion.d.ts
@@ -0,0 +1,7 @@
+declare module "framer-motion" {
+  export const motion: any;
+  export const useReducedMotion: any;
+  export const useScroll: any;
+  export const useTransform: any;
+  export type MotionValue<T = any> = any;
+}
