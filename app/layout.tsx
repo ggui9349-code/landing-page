3a251ab2cdc309ab6498dd98532caae9eb0696d5
ggui9349-code <ggui9349@gmail.com>
2026-09-13 01:00:46 -0300

    feat: add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { siteConfig } from "@/lib/seo";
 import "./globals.css";
 
 const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?? "G-91H1L6NX23";
+const META_PIXEL_ID = "712490391806172";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -122,9 +123,25 @@ export default function RootLayout({
           <Script id="google-analytics">
             {`window.dataLayer = window.dataLayer || []; function gtag(){window.dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${GA_MEASUREMENT_ID}');`}
           </Script>
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,
+'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${META_PIXEL_ID}');fbq('track','PageView');`}
+          </Script>
         </head>
       }
       <body className={`${inter.variable} ${manrope.variable}`}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
